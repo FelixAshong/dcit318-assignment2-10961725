33af3b10df55f259fba71665a5906a3d1c0ee62e
FelixAshong <felixashong4@gmail.com>
2024-06-14 08:52:22 +0000

    Initial commit with InheritanceAndMethodOverriding project files

---
+++ AbstractClassesAndMethods/AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Circle myCircle = new Circle(5.0);
+        Rectangle myRectangle = new Rectangle(4.0, 6.0);
+
+        Console.WriteLine($"Area of Circle: {myCircle.GetArea()}");
+        Console.WriteLine($"Area of Rectangle: {myRectangle.GetArea()}");
+    }
+}
---
+++ InheritanceAndMethodOverriding/InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
