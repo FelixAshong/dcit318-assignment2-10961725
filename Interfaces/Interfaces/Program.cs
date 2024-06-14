4d862ae75c14eaf89deb14e6ea0a5f1056d38c35
FelixAshong <felixashong4@gmail.com>
2024-06-14 08:57:36 +0000

    Completed Task 3: Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        IMovable myCar = new Car();
+        IMovable myBicycle = new Bicycle();
+
+        myCar.Move();
+        myBicycle.Move();
+    }
+}
